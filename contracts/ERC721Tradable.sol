041de6430f80f439ba965fee769a1f469281c0e9
sakelle <sakelle@amazon.com>
2021-03-07 18:48:40 -0800

    removing the onlyOwner description from the mintTo in order to allow anyone to mint NFTs in our collection

--- contracts/ERC721Tradable.sol
+++ contracts/ERC721Tradable.sol
@@ -32,7 +32,7 @@ contract ERC721Tradable is ERC721Full, Ownable {
      * @dev Mints a token to an address with a tokenURI.
      * @param _to address of the future owner of the token
      */
-    function mintTo(address _to) public onlyOwner {
+    function mintTo(address _to) public {
         uint256 newTokenId = _getNextTokenId();
         _mint(_to, newTokenId);
         _incrementTokenId();
